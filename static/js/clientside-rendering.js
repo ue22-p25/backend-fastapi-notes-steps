e1755f96628a3ab8e1ff59c8efa5c27dbde11235
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-09-26 17:59:25 +0200

    step 13c - CSR on note creation

--- static/js/clientside-rendering.js
+++ static/js/clientside-rendering.js
@@ -1,5 +1,19 @@
 function createNoteElement(note) {
-    console.log("Creating note:", note);
+    const elt = document.createElement("li");
+    elt.classList.add("note");
+    elt.id = `note-${note.id}`;
+    const html = `
+    <div class="title">${note.title}
+        <button class="delete-button" onclick="note_delete(this, ${note.id})">🗑</button>
+    </div>
+    <div class="line">
+        <span>${note.description}</span>
+        <input type="checkbox" ${note.done ? "checked" : ""}
+        onchange="note_done_changed(this, ${note.id})" />
+    </div>
+`
+    elt.innerHTML = html;
+    document.querySelector("ul.notes").appendChild(elt);
 }
 
 function updateNoteElement(note) {
--- templates/notes.html.j2-step.md
+++
@@ -1,7 +0,0 @@
-## keep track of note ids in the HTML
-
-with this simple change, we can now keep track of the ids of the notes in the
-HTML tree
-
-this will come in handy in the next steps, when we will need to actually apply
-changes (received on the ws channel) to the corresponding notes in the DOM.
